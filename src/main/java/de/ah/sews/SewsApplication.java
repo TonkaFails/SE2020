4d8f504e312117aeac02beb0aa42d364cc80672c
Anton Ludwig Hofmeier <anton.hofmeier@hm.edu>
2021-01-07 18:00:29 +0100

    Initial commit

---
+++ src/main/java/de/ah/sews/SewsApplication.java
@@ -0,0 +1,13 @@
+package de.ah.sews;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SewsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SewsApplication.class, args);
+	}
+
+}
